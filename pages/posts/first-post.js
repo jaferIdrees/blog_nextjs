8d2974206add2592a19864fd3f9f3633e466ad03
jaferidrees <jaferll@gmail.com>
2022-12-27 19:44:02 +0100

    Update first_post page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const FirstPost = () => {
-    return <h1>First post</h1>
+    return (
+        <>
+            <h1>First post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    )
 }
 
 export default FirstPost;

ad276feb10c541357422e8a4d8ae68358b86b7b3
jaferidrees <jaferll@gmail.com>
2022-12-30 07:31:26 +0100

    update first-post file

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,25 +1,18 @@
 import Link from "next/link";
 import Head from 'next/head';
-import Script from "next/script";
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
-            <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() =>
-                    console.log(`script loaded correctly, window.FB has been populated`)
-                }
-            />
             <h1>First post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
 

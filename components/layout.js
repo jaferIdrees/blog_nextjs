94876665952b50dc9690da0ef42892623249e152
jaferidrees <jaferll@gmail.com>
2022-12-30 07:41:39 +0100

    Use css modules to style text

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,7 @@
+import styles from './layout.module.css'
+
 const Layout = ({children}) => {
-    return <div>{children}</div>
+    return <div className={styles.container} >{children}</div>
 }
 
 export default Layout;

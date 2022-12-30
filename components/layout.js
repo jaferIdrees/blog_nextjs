5639cf2579f8e1ffca08a80a7561c73f28d977b6
jaferidrees <jaferll@gmail.com>
2022-12-30 08:27:51 +0100

    polish styling

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
+const name = 'Jafer Yousef';
 export const siteTitle = 'Next.js Sample Website';
 
 const Layout = ({ children, home }) => {

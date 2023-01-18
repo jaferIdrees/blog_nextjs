7ca0e8d745c80562265d9dbc91a1fc41063c761c
jaferidrees <jaferll@gmail.com>
2023-01-18 20:24:49 +0100

    update [id].js file

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,14 @@
 import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();

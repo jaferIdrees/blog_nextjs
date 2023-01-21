69a6ad3da6faaa487681c611ee611e5b3218e9b3
jaferidrees <jaferll@gmail.com>
2023-01-21 20:13:37 +0100

    update post function to use Date component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -22,14 +23,14 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-       <Head>
+      <Head>
         <title>{postData.title}</title>
       </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
